141ef4ee3a0d9bfa1d13b664a8c4c1f456858c2d
Doğukan Terzi <dogukanterzi@gmail.com>
2024-01-14 01:44:47 +0300

    init

---
+++ src/main/java/com/configuration/PicVistaconfigurationserver/PicVistaConfigurationServerApplication.java
@@ -0,0 +1,13 @@
+package com.configuration.PicVistaconfigurationserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicVistaConfigurationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicVistaConfigurationServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/configuration/PicVistaconfigurationserver/PicVistaConfigurationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.configuration.PicVistaconfigurationserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicVistaConfigurationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
